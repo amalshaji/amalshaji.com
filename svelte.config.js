3b974aca2a9fdf4ae56df78e39c2e7c2a5646f94
Amal Shaji <18011385+amalshaji@users.noreply.github.com>
2021-11-15 17:23:42 +0000

    🚀 setup static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,19 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	},
 
 	preprocess: [
