b5d7d4475ae3c4e02bee7e80b7d95fb826546607
Amal Shaji <18011385+amalshaji@users.noreply.github.com>
2021-11-16 04:39:57 +0000

    🐛 fix seo url

--- src/lib/components/SEO.svelte
+++ src/lib/components/SEO.svelte
@@ -15,11 +15,11 @@
 <meta property="og:url" content={url} />
 <meta property="og:title" content={title} />
 <meta property="og:description" content={description} />
-<meta property="og:image" content={`${url}${image}`} />
+<meta property="og:image" content={`https://amalshajid.netlify.app${image}`} />
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:url" content={url} />
 <meta property="twitter:title" content={title} />
 <meta property="twitter:description" content={description} />
-<meta property="twitter:image" content={`${url}${image}`} />
+<meta property="twitter:image" content={`https://amalshajid.netlify.app${image}`} />
