0a93193100d4eaf989adb41e242ada4bbf6e1088
Amal Shaji <18011385+amalshaji@users.noreply.github.com>
2021-11-17 11:37:26 +0000

    💄 update hero ui

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -3,22 +3,22 @@
 		<div class="container mx-auto px-4">
 			<div class="w-full px-4 mb-12 md:mb-20 mx-auto lg:mb-0 flex flex-col items-center">
 				<h2 class="mb-4 text-4xl lg:text-5xl font-bold mx-auto font-heading">
-					<span class="dark:text-gray-50">Hello 👋</span>
-					<span class="text-indigo-600">I'm Amal</span>
+					<span class="dark:text-gray-300">Hello 👋</span>
+					<span class="text-indigo-600 dark:text-yellow-700">I'm Amal</span>
 				</h2>
-				<div class="max-w-md mx-1 lg:mx-0">
-					<p class="mb-6 text-gray-900 dark:text-gray-50 leading-loose">
+				<div class="max-w-lg mx-1 lg:mx-0">
+					<p class="mb-6 text-gray-900 dark:text-gray-50 leading-loose text-justify">
 						I love working with Python, Go and Svelte. Once in a while, I try to experiment on
-						something and document my journey here. Lately I'm into Rust and Solana.
+						something and document my journey here. I'm also interested in Rust and Solana.
 						<br />
 						Building this website using Sveltekit and tailwindCSS is one of my latest side projects.
 					</p>
 					<div class="text-center">
 						<a
-							class="inline-block mb-3 lg:mb-0 lg:mr-3 w-full lg:w-auto py-2 px-6 leading-loose bg-indigo-600 hover:bg-yellow-500 text-white font-semibold rounded-l-xl rounded-t-xl transition duration-200"
+							class="inline-block mb-3 lg:mb-0 lg:mr-3 w-full lg:w-auto py-2 px-6 leading-loose bg-indigo-600  text-white dark:bg-yellow-700 font-semibold rounded-l-xl rounded-t-xl transition duration-200"
 							href="/blog">Blog</a
 						><a
-							class="inline-block w-full lg:w-auto py-2 px-6 leading-loose font-semibold bg-gray-800 text-white dark:text-black dark:bg-white hover:bg-gray-50 hover:border rounded-l-xl rounded-t-xl transition duration-200"
+							class="inline-block w-full lg:w-auto py-2 px-6 leading-loose font-semibold bg-gray-800 text-white dark:text-black dark:bg-gray-300 rounded-l-xl rounded-t-xl transition duration-200"
 							href="/projects">Projects</a
 						>
 					</div>
