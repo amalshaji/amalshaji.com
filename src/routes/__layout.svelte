6e92f474df111ee388fdc0a89018796d1c04b70d
Amal Shaji <18011385+amalshaji@users.noreply.github.com>
2021-11-29 15:52:54 +0000

    Add page progress bar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,6 +6,25 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import { theme } from '$lib/store';
 	import { onMount } from 'svelte';
+	import NProgress from 'nprogress';
+	import { navigating } from '$app/stores';
+
+	// NProgress css
+	import 'nprogress/nprogress.css';
+
+	NProgress.configure({
+		// Full list: https://github.com/rstacruz/nprogress#configuration
+		minimum: 0.16
+	});
+
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
 
 	onMount(() => {
 		$theme = localStorage.getItem('amalshaji_com_theme') || 'light';
