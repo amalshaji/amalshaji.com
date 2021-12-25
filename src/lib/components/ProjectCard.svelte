a9c6313ca1dbb88c69cd10f3821303ea32af28d3
Amal Shaji <amalshajid@gmail.com>
2021-12-25 09:16:37 +0000

    Update projects layout

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let project;
+	import Date from './Date.svelte';
+</script>
+
+<div class="p-2">
+	<a href={project.metadata.link} target="_blank" rel="noreferrer">
+		{#if project.metadata.image}
+			<div class="relative">
+				<img
+					src={project.metadata.image}
+					alt={project.metadata.title}
+					class="rounded-lg shadow-sm w-full h-64 object-cover"
+				/>
+				<div
+					class="absolute top-4 right-4 rounded shadow bg-white text-gray-900 dark:bg-gray-900 dark:text-white text-sm px-2 py-0.5"
+				>
+					{#if project.metadata.date}
+						<Date date={project.metadata.date} />
+					{/if}
+				</div>
+			</div>
+		{/if}
+		<div class="my-2 text-xl font-semibold">{project.metadata.title}</div>
+		{#if project.metadata.description}
+			<div class="">{project.metadata.description}</div>
+		{/if}
+	</a>
+</div>
--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -22,6 +22,7 @@
 
 <script>
 	import Seo from '$lib/components/SEO.svelte';
+	import ProjectCard from '$lib/components/ProjectCard.svelte';
 	export let projects;
 </script>
 
@@ -30,51 +31,8 @@
 	<Seo title="Projects | Amal Shaji" url="/projects" />
 </svelte:head>
 
-<div class="mx-6 my-2 md:my-8 md:mx-auto lg:w-3/4">
-	<div class="text-2xl md:text-4xl font-semibold my-4 dark:text-gray-300">All Projects</div>
-	<div class="block md:flex md:flex-wrap overflow-hidden lg:-mx-4 xl:-mx-4">
-		{#each projects as project}
-			<div
-				class="w-full my-2 border md:border-0 dark:border-gray-700 rounded-lg md:w-1/2 overflow-hidden lg:my-4 lg:px-4 lg:w-1/2 xl:my-4 xl:px-4 xl:w-1/2"
-			>
-				<h3 class="text-lg md:text-2xl font-semibold leading-8 tracking-tight">
-					<a
-						href={`/projects/${project.path.replace('.md', '').replace('.svx', '')}`}
-						class="text-gray-900 dark:text-gray-400"
-					>
-						<div class="w-full h-56 md:h-72 lg:h-80">
-							<img
-								src={project.metadata.image}
-								loading="lazy"
-								class="my-2 mx-auto sm:mx-0 rounded-lg h-full"
-								alt={project.metadata.title}
-							/>
-						</div>
-						<div class="flex mx-4">
-							<p class="dark:text-gray-300">{project.metadata.title}</p>
-							<a href={project.metadata.link} class="mx-1" target="_blank" rel="noreferrer">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									class="h-6 w-6"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-									/>
-								</svg></a
-							>
-						</div>
-					</a>
-				</h3>
-				<div class="prose mx-4 my-2 text-sm md:text-lg text-gray-500 max-w-none dark:text-gray-400">
-					{project.metadata.description}
-				</div>
-			</div>
-		{/each}
-	</div>
+<div class="container p-6 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8">
+	{#each projects as project}
+		<ProjectCard {project} />
+	{/each}
 </div>
