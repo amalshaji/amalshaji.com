dc11847dd878379e98b086d9ef698aa4b6da5053
Amal Shaji <amalshajid@gmail.com>
2022-01-23 09:19:44 +0000

    📝 Add career and tech stack to about

---
+++ src/lib/components/Career.svelte
@@ -0,0 +1,65 @@
+<section class="my-4">
+	<div
+		class="flex my-4 transform hover:-translate-y-2 duration-150 flex-row items-center bg-white rounded-lg border hover:shadow-md md:max-w-xl  dark:border-gray-700 dark:bg-gray-800"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-32 w-32 rounded-none rounded-l-lg text-teal-600"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
+			/>
+		</svg>
+		<div class="flex flex-col justify-between p-4 leading-normal">
+			<h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+				Software Development Engineer
+			</h5>
+			<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+				Cannot reveal the company. Working on a secret project (Jason Bourne level).
+			</p>
+			<p class="-top-1 right-2 md:absolute bg-yellow-400 w-36 md:w-auto shadow-md rounded-md px-2">
+				2021 - present
+			</p>
+		</div>
+	</div>
+	<div
+		class="flex flex-row my-4 transform hover:-translate-y-2 duration-150 items-center bg-white rounded-lg border hover:shadow-md md:max-w-xl  dark:border-gray-700 dark:bg-gray-800"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-24 w-24 rounded-none rounded-l-lg text-cyan-600"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+			/>
+		</svg>
+		<div class="flex flex-col justify-between p-4 leading-normal">
+			<h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+				Freelance Writer (Software)
+			</h5>
+			<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+				I write, mostly for <a
+					href="https://testdriven.io/authors/shaji"
+					target="_blank"
+					rel="noreferrer"
+					class="hover:underline">testdriven.io</a
+				>
+			</p>
+			<p class="-top-1 right-2 md:absolute bg-yellow-400 w-36 md:w-auto shadow-md rounded-md px-2">
+				2020 - present
+			</p>
+		</div>
+	</div>
+</section>
