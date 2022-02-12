fd017f539bbaa372ad25bcdeca58757757ad7de4
Amal Shaji <amalshajid@gmail.com>
2022-02-12 19:47:45 +0530

    🔥 Update index page

---
+++ src/lib/components/Links.svelte
@@ -0,0 +1,51 @@
+<script>
+	import LinkCard from './LinkCard.svelte';
+	let links = [
+		{
+			text: 'Blog',
+			description: 'I write about what my weird experiments',
+			link: '/blog',
+			external: false
+		},
+		{
+			text: 'About',
+			description: 'About me',
+			link: '/about',
+			external: false
+		},
+		{
+			text: 'Twitter',
+			description: 'My twitter account. Updates about my latest posts.',
+			link: 'https://twitter.com/pydantic',
+			external: true
+		},
+		{
+			text: 'GitHub',
+			description: 'Home for all my experiments.',
+			link: 'https://github.com/amalshaji',
+			external: true
+		}
+	];
+</script>
+
+<div class="flex pt-12 justify-center">
+	<div class="relative w-24">
+		<!-- svelte-ignore a11y-img-redundant-alt -->
+		<img
+			class="rounded-full border border-gray-200 shadow-sm"
+			src="/images/cat.jpeg"
+			alt="user image"
+		/>
+		<p class="mt-4 text-center font-semibold text-xl">Amal</p>
+	</div>
+</div>
+
+<div class="flex pb-12 justify-center">
+	<span class="mt-2 bg-blue-500 px-4 py-1 text-white rounded-lg font-medium text-sm"
+		>Learning to master software</span
+	>
+</div>
+
+{#each links as link}
+	<LinkCard data={link} />
+{/each}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,31 +1,6 @@
-<script context="module">
-	const allPosts = import.meta.glob('./blog/*{md,svx}');
-
-	let body = [];
-	for (let path in allPosts) {
-		body.push(
-			allPosts[path]().then(({ metadata }) => {
-				return { path, metadata };
-			})
-		);
-	}
-	export const load = async () => {
-		let posts = await Promise.all(body);
-		posts.sort((a, b) => (a.metadata.date > b.metadata.date ? -1 : 1));
-		posts = posts.slice(0, 3);
-		return {
-			props: {
-				posts
-			}
-		};
-	};
-</script>
-
 <script>
-	import Hero from '$lib/components/Hero.svelte';
 	import Seo from '$lib/components/SEO.svelte';
-	import BlogCard from '$lib/components/BlogCard.svelte';
-	export let posts;
+	import Links from '$lib/components/Links.svelte';
 </script>
 
 <svelte:head>
@@ -33,12 +8,4 @@
 	<Seo />
 </svelte:head>
 
-<Hero />
-
-<h1 class="container px-6 pt-6 mx-auto text-3xl font-semibold">Latest posts</h1>
-
-<div class="container p-6 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8">
-	{#each posts as post}
-		<BlogCard {post} />
-	{/each}
-</div>
+<Links />
